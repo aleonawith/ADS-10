7f04a20cccc355a87a6e028ecbbd80b3ef0877c4
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-06-01 18:12:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,19 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::vector<char> g;
+  std::string perm = tree.gPermut(n);
+  if (perm.length() == 0) {
+    return g;
+  }
+  g.resize(perm.length(), ' ');
+  int i = 0;
+  while (true) {
+    if (perm.length() == 0) {
+      return g;
+    } else {
+      g[i++] = perm[0];
+      perm.erase(perm.begin());
+    }
+  }
 }
